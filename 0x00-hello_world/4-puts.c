cf97fa2a24c2002ede8faa3a8de2b92ec38a3bb5
Malik2501 <olatunjimalik6@gmail.com>
2023-08-10 11:26:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #Include < stdio.h >
 /**
  * main - Entry point
- * Return: 0 (Succes)
+ * Return: 0 (Succesi)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
