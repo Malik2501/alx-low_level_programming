8cb75f63529c7b65d2e60535e2bed0b3a0a23362
Malik2501 <olatunjimalik6@gmail.com>
2023-08-12 07:00:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+		char c;
+
+		c = 'a';
+		while
+			(c <= 'a') {
+				if ((c != 'q' && c != 'e') && c <= 'z')
+					putchar(c);
+				c++;
+			}
+		putchar('\n');
+		return (0);
+}
