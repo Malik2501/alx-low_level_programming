ea7ce757c1ed35e45f2dbcde3f3b6420c6eec491
Malik2501 <olatunjimalik6@gmail.com>
2023-08-16 11:00:47 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main: List the sum of all the natural numbers below 1024 (excluded)
- * that are multiples of 3 or 5.
+ * main: prints the sum of all the multiples of 3 or 5 below 1024 (excluded)
  *
  * Return: Always 0.
  */
@@ -12,7 +11,7 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (1 % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 
