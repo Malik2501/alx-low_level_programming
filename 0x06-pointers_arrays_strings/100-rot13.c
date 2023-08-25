dacc10c74627514345e8c231640eea830bff8b6d
Malik2501 <olatunjimalik6@gmail.com>
2023-08-24 19:58:48 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,12 +8,12 @@
  */
 
 
-char *rot13(char s)
+char *rot13(char *s)
 {
 	int i, j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
