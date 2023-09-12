529adcd9be781bcf3411ffa130edaa1c36c27a9a
Malik2501 <olatunjimalik6@gmail.com>
2023-09-12 00:29:25 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is my readme file on 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - a struct that creates dog info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: my  first struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
