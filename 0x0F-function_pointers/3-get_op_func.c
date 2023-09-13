679eb9d0d252b9fe782f8c59d0f3ef5b542ba669
Malik2501 <olatunjimalik6@gmail.com>
2023-09-13 16:04:22 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 /**
- * get_op_func - select the current function to perform
+ * get_op_func - select the correct function to perform
  * the operation asked by the user
  * @s: the operation passed as argument
  * Return: a pointer to the function corresponding
